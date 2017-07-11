c302575edbf15a03d4e17bcafc43e504fc7cc678
Jason R. Carrete <jasoncarrete5@gmail.com>
2017-07-11 18:25:23 -0400

    Declared some structs necessary for the linkedlist

--- src/cds/headers/cds_linkedlist.h
+++ src/cds/headers/cds_linkedlist.h
@@ -3,3 +3,20 @@
  *
  * Author: Jason R. Carrete
  */
+
+#pragma once
+
+typedef struct cds_LinkedNode {
+	struct cds_LinkedNode *prev;
+	struct cds_LinkedNode *next;
+	void *data;
+} cds_LinkedNode;
+
+typedef struct {
+	int size;
+	cds_LinkedNode *head;
+	cds_LinkedNode *tail;
+} cds_LinkedList;
+
+cds_LinkedList *cds_createLinkedList();
+void cds_deleteLinkedList(cds_LinkedList *list);
