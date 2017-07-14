6ded4edb84d028b3e233cd01b5a9cb16910adea5
Jason R. Carrete <jasoncarrete5@gmail.com>
2017-07-13 22:40:50 -0400

    Implemented creation of a LinkedList and tested it

--- src/cds/c/linkedlist.c
+++ src/cds/c/linkedlist.c
@@ -2,4 +2,18 @@
  * Author: Jason R. Carrete
  */
  
+#include <stdlib.h>
 #include "CDS/linkedlist.h"
+
+cds_LinkedList *cds_createLinkedList() {
+    cds_LinkedList *list = malloc(sizeof(cds_LinkedList));
+    list->size = 0;
+    list->head = NULL;
+    list->tail = NULL;
+    return list;
+}
+
+void cds_deleteLinkedList(cds_LinkedList *list) {
+    // TODO recursivly free LinkedNodes
+    free(list);
+}
--- src/cdsTest/c/test_create_linked_list.c
+++ src/cdsTest/c/test_create_linked_list.c
@@ -2,5 +2,9 @@
 #include "CDS/linkedlist.h"
 
 void test_create_linked_list() {
-	
+	cds_LinkedList *list = cds_createLinkedList();
+	printf("Created LinkedList: %p\n", list);
+	cds_deleteLinkedList(list);
+	printf("Deleted LinkedList: %p\n", list);
+	//test
 }
