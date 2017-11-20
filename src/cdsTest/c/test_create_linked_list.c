74bfc4339ccc75b0ebc547c752fb75264e8204d8
Jason Carrete <jasoncarrete5@gmail.com>
2017-11-20 17:47:58 -0500

    Fixed test case to call proper function to create a linked list

--- src/cdsTest/c/test_create_linked_list.c
+++ src/cdsTest/c/test_create_linked_list.c
@@ -2,7 +2,8 @@
 #include "CDS/linkedlist.h"
 
 void test_create_linked_list() {
-	cds_LinkedList *list = cds_createLinkedList();
+	cds_LinkedList *list = malloc(sizeof(cds_LinkedList));
+	cds_createLinkedList(list);
 	printf("Created LinkedList: %p\n", list);
 	cds_deleteLinkedList(list);
 	printf("Deleted LinkedList: %p\n", list);
