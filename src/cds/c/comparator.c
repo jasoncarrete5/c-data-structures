7600032d51315e230c9d28aa42eccf79b62d1402
Jason Carrete <jasoncarrete5@gmail.com>
2018-02-15 17:21:37 -0500

    Setup base template for comparator.c

---
+++ src/cds/c/comparator.c
@@ -0,0 +1,54 @@
+/*
+ * comparator.c
+ *
+ * Author: Jason R. Carrete
+ */
+
+#include "CDS/comparator.h"
+
+/*
+ * Lexicographical char comparator
+ */
+int cds_CharComparator(void *a, void *b) {
+	
+}
+
+int cds_ShortComparator(void *a, void *b) {
+
+}
+
+int cds_IntComparator(void *a, void *b) {
+
+}
+
+int cds_LongIntComparator(void *a, void *b) {
+
+}
+
+int cds_LongLongComparator(void *a, void *b) {
+
+}
+
+int cds_UCharComparator(void *a, void *b) {
+
+}
+
+int cds_UShortComparator(void *a, void *b) {
+
+}
+
+int cds_UIntComparator(void *a, void *b) {
+
+}
+
+int cds_ULongIntComparator(void *a, void *b) {
+
+}
+
+int cds_ULongLongComparator(void *a, void *b) {
+
+}
+
+int cds_CStringComparator(void *a, void *b) {
+
+}
